0929d71aa47bee67846bfbc9e9a8745292336077
Mateusz Drażyk <drazyk.mateusz@gmail.com>
2020-09-06 08:56:18 +0200

    Add MVP for downloading video from new facebook layout

--- content-script.js
+++ content-script.js
@@ -1,160 +1,73 @@
-let videos;
-const ELEMENT_ID = 'custom_context_menu_item';
+const CONTEXT_MENU_ID = 'custom-context-menu';
 
-const getFromBetween = {
-  results: [],
-  string: '',
-  getFromBetween: function(sub1, sub2) {
-    if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0)
-      return false;
-    const SP = this.string.indexOf(sub1) + sub1.length;
-    const string1 = this.string.substr(0, SP);
-    const string2 = this.string.substr(SP);
-    const TP = string1.length + string2.indexOf(sub2);
-    return this.string.substring(SP, TP);
-  },
-  removeFromBetween: function(sub1, sub2) {
-    if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0)
-      return false;
-    const removal = sub1 + this.getFromBetween(sub1, sub2) + sub2;
-    this.string = this.string.replace(removal, '');
-  },
-  getAllResults: function(sub1, sub2) {
-    if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return;
+let contextMenuOpen = false;
 
-    const result = this.getFromBetween(sub1, sub2);
-    this.results.push(result);
-    this.removeFromBetween(sub1, sub2);
+const createDownloadButton = () => {
+  const button = document.createElement('button');
 
-    if (this.string.indexOf(sub1) > -1 && this.string.indexOf(sub2) > -1) {
-      this.getAllResults(sub1, sub2);
-    } else return;
-  },
-  get: function(string, sub1, sub2) {
-    this.results = [];
-    this.string = string;
-    this.getAllResults(sub1, sub2);
-    return this.results;
-  },
+  const handleClick = async () => {
+    const { href } = window.location;
+    const url = href.replace('www.', 'd.');
+
+    chrome.runtime.sendMessage({
+      type: 'DOWNLOAD_VIDEO',
+      url,
+    });
+  };
+
+  // button props
+  button.innerText = 'Download';
+
+  // button style
+  button.style.width = '100px';
+  button.style.height = '30px';
+  button.style.position = 'relative';
+  button.style.top = '60px';
+  button.style.left = '100px';
+
+  // button handler
+  button.onclick = handleClick;
+
+  return button;
 };
 
-const getVideoLink = htmlData => {
-  const [sdUrl] = getFromBetween.get(htmlData, 'sd_src:"', '",hd_tag:"');
-  const [hdUrl] = getFromBetween.get(htmlData, 'hd_src:"', '",sd_src:"');
-  const [content] = getFromBetween.get(
-    htmlData,
-    '<meta property="og:video:secure_url" content="',
-    '" />',
-  );
+const createContextMenuElement = () => {
+  const [body] = document.getElementsByTagName('body');
 
-  if (sdUrl || hdUrl) {
-    return sdUrl || hdUrl;
-  }
+  const contextMenu = document.createElement('div');
 
-  const div = document.createElement('div');
-  div.innerHTML = content;
-  const decodedUrl = div.firstChild.nodeValue;
-  return decodedUrl;
+  // context menu props
+  contextMenu.setAttribute('id', CONTEXT_MENU_ID);
+
+  // context menu style
+  contextMenu.style.width = '300px';
+  contextMenu.style.height = '150px';
+  contextMenu.style.backgroundColor = 'white';
+  contextMenu.style.top = '0px';
+  contextMenu.style.left = '0px';
+  contextMenu.style.position = 'absolute';
+  contextMenu.style.display = 'none';
+
+  contextMenu.appendChild(createDownloadButton());
+
+  body.appendChild(contextMenu);
 };
 
-const downloadVideo = async videoLink => {
-  const htmlData = await fetch(videoLink).then(data => data.text());
-  const url = getVideoLink(htmlData);
+const openHideContextMenu = ({ xMousePosition, yMousePosition }) => {
+  const contextMenu = document.getElementById(CONTEXT_MENU_ID);
 
-  chrome.runtime.sendMessage({
-    type: 'VIDEO_URLS',
-    url,
-  });
+  contextMenuOpen = !contextMenuOpen;
+
+  contextMenu.style.display = contextMenuOpen ? 'flex' : 'none';
+  contextMenu.style.top = `${yMousePosition}px`;
+  contextMenu.style.left = `${xMousePosition}px`;
 };
 
-const getContextMenuContainers = () =>
-  new Promise((resolve, reject) => {
-    try {
-      setTimeout(() => {
-        const contextMenuContainers = document.querySelectorAll(
-          'ul[role="menu"]',
-        );
-        resolve(contextMenuContainers);
-      }, 10);
-    } catch (error) {
-      reject('Cannot find context menu container');
-    }
-  });
-
-const createDownloadButtonElement = contextMenuContainers => {
-  return new Promise((resolve, reject) => {
-    try {
-      for (const contextMenuContainer of contextMenuContainers) {
-        const lastChild = contextMenuContainer.lastChild;
-        const spanElement = lastChild.querySelector('[value]');
-        const videoLink = spanElement && spanElement.getAttribute('value');
-
-        const elementId = 'custom_context_menu_item';
-        const customContextMenuItem = contextMenuContainer.querySelector(
-          `#${elementId}`,
-        );
-        if (customContextMenuItem) {
-          customContextMenuItem.remove();
-        }
-
-        const li = lastChild.cloneNode(true);
-
-        li.id = ELEMENT_ID;
-        li.lastChild.innerHTML = 'Download Video';
-        li.onclick = () => {
-          contextMenuContainer.parentNode.parentNode.parentNode.parentNode.classList.add(
-            'hidden_elem',
-          );
-
-          downloadVideo(videoLink);
-        };
-        li.onmouseover = e => {
-          e.target.style.backgroundColor = '#4262ae';
-          e.target.style.color = '#ffffff';
-          e.target.style.borderColor = '#2b487d';
-        };
-        li.onmouseout = e => {
-          e.target.style.backgroundColor = '#ffffff';
-          e.target.style.color = '#000000';
-          e.target.style.borderColor = '#ffffff';
-        };
-        contextMenuContainer.appendChild(li);
-      }
-      resolve();
-    } catch (error) {
-      reject(error);
-    }
-  });
+window.onload = () => {
+  createContextMenuElement();
 };
 
-const addContextMenuToVideo = async videos => {
-  for (const video of videos) {
-    video.oncontextmenu = async () => {
-      try {
-        const contextMenuContainers = await getContextMenuContainers();
-        await createDownloadButtonElement(contextMenuContainers);
-      } catch (error) {
-        console.error('Something went wrong.', error);
-      }
-    };
-
-    video.onplay = async () => {
-      try {
-        const contextMenuContainers = await getContextMenuContainers();
-        await createDownloadButtonElement(contextMenuContainers);
-      } catch (error) {
-        console.error('Something went wrong.', error);
-      }
-    };
-  }
+window.oncontextmenu = (event) => {
+  const { x: xMousePosition, y: yMousePosition } = event;
+  openHideContextMenu({ xMousePosition, yMousePosition });
 };
-
-(async () => {
-  videos = document.getElementsByTagName('video');
-  await addContextMenuToVideo(videos);
-})();
-
-window.addEventListener('scroll', async () => {
-  videos = document.getElementsByTagName('video');
-  await addContextMenuToVideo(videos);
-});
--- index.js
+++ index.js
@@ -1,19 +1,64 @@
 let tabId = null;
+
+const getFromBetween = {
+  results: [],
+  string: '',
+  getFromBetween: function (sub1, sub2) {
+    if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0)
+      return false;
+    const SP = this.string.indexOf(sub1) + sub1.length;
+    const string1 = this.string.substr(0, SP);
+    const string2 = this.string.substr(SP);
+    const TP = string1.length + string2.indexOf(sub2);
+    return this.string.substring(SP, TP);
+  },
+  removeFromBetween: function (sub1, sub2) {
+    if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0)
+      return false;
+    const removal = sub1 + this.getFromBetween(sub1, sub2) + sub2;
+    this.string = this.string.replace(removal, '');
+  },
+  getAllResults: function (sub1, sub2) {
+    if (this.string.indexOf(sub1) < 0 || this.string.indexOf(sub2) < 0) return;
+
+    const result = this.getFromBetween(sub1, sub2);
+    this.results.push(result);
+    this.removeFromBetween(sub1, sub2);
+
+    if (this.string.indexOf(sub1) > -1 && this.string.indexOf(sub2) > -1) {
+      this.getAllResults(sub1, sub2);
+    } else return;
+  },
+  get: function (string, sub1, sub2) {
+    this.results = [];
+    this.string = string;
+    this.getAllResults(sub1, sub2);
+    return this.results;
+  },
+};
+
 chrome.tabs.query(
   { active: true, currentWindow: true },
-  tabs => (tabId = tabs[0].id),
+  (tabs) => (tabId = tabs[0].id),
 );
 
 chrome.runtime.onMessage.addListener(({ type, url }) => {
-  if (type === 'VIDEO_URLS') {
-    const link = document.createElement('a');
+  if (type === 'DOWNLOAD_VIDEO') {
     fetch(url)
-      .then(video => video.url)
-      .then(downloadableUrl => {
-        link.href = downloadableUrl;
-        link.download = true;
-        link.click();
-        link.remove();
+      .then((data) => data.text())
+      .then((htmlText) => {
+        const [videoUrl] = getFromBetween.get(
+          htmlText,
+          'video_redirect/?src=',
+          '&amp;',
+        );
+
+        const url = decodeURIComponent(videoUrl);
+
+        // TODO wywalic setTimeout
+        setTimeout(() => {
+          chrome.downloads.download({ url });
+        }, 1000);
       });
   }
 });
